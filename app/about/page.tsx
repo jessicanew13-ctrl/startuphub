5c2c5009824931bd70f5621580831223242d9db8
unknown <lilkem303@gmail.com>
2026-04-02 14:44:44 +0000

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {        
+
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptatem provident ipsam quos. Possimus neque illo odio aliquid sed eum quaerat corporis enim repellat quidem, consequatur fugit veritatis blanditiis nostrum?</h1>
+        </div>
+    )
+}
